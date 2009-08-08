ac1cff786d5574a4077c8ecd2e6794dc5c02a837
Aaron Patterson <aaron.patterson@gmail.com>
2009-08-07 21:34:59 -0700

    getting the uiUidLen

--- ext/nfc/nfc_iso14443a.c
+++ ext/nfc/nfc_iso14443a.c
@@ -2,7 +2,17 @@
 
 VALUE cNfcISO14443A;
 
+static VALUE uiUidLen(VALUE self)
+{
+  tag_info_iso14443a * tag;
+  Data_Get_Struct(self, tag_info_iso14443a, tag);
+
+  return INT2NUM(tag->uiUidLen);
+}
+
 void init_iso14443a()
 {
   cNfcISO14443A = rb_define_class_under(cNfc, "ISO14443A", rb_cObject);
+
+  rb_define_method(cNfcISO14443A, "uiUidLen", uiUidLen, 0);
 }
