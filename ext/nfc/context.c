7d699c04b8adb24a5ef9010a18762270c906e15d
Aaron Patterson <aaron.patterson@gmail.com>
2013-05-25 21:41:43 -0700

    can find arbitrary length

--- ext/nfc/context.c
+++ ext/nfc/context.c
@@ -6,20 +6,23 @@ static VALUE allocate(VALUE klass) {
   return Data_Wrap_Struct(klass, NULL, nfc_exit, context);
 }
 
-static VALUE devices(VALUE self)
+static VALUE devices(VALUE self, VALUE len)
 {
   nfc_context *ctx;
-  nfc_connstring strs[10];
+  nfc_connstring * strs;
   size_t found, i;
   VALUE devs;
 
   Data_Get_Struct(self, nfc_context, ctx);
+
+  strs = malloc(sizeof(nfc_connstring) * len);
 
   found = nfc_list_devices(ctx, strs, 10);
   devs = rb_ary_new2(found);
   for (i = 0; i < found; i++) {
     rb_ary_push(devs, rb_str_new2(strs[i]));
   }
+  free(strs);
   return devs;
 }
 
@@ -28,5 +31,5 @@ void init_context()
   VALUE cContext = rb_define_class_under(mNfc, "Context", rb_cObject);
   rb_define_alloc_func(cContext, allocate);
 
-  rb_define_method(cContext, "devices", devices, 0);
+  rb_define_method(cContext, "devices", devices, 1);
 }
